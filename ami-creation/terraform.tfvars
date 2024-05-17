c29361639737f8382770eb2900caec1a993c2bf6
Gad ndayishimiye <nday.gady@gmail.com>
2024-05-16 18:35:10 -0700

    devops-auto-links
    Signed-off-by: Gad ndayishimiye <nday.gady@gmail.com>

---
+++ ami-creation/terraform.tfvars
@@ -0,0 +1,3 @@
+region = "us-east-1"
+source_id = "ami-0e329ed9b19b23b29"
+ami_name = "jenkins_ami"
---
+++ ami-creation/variables.tf
@@ -0,0 +1,10 @@
+variable "source_id" {
+  default = "ami-0e329ed9b19b23b29"
+}
+variable "ami_name" {
+  default = "jenkins_ami"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
